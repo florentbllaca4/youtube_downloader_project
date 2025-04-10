a6c5356e1c6258b1a8160e0a3808f468b4319d39
florentbllaca4 <bllacaflorent31@gmail.com>
2025-04-10 14:36:20 +0200

    Update index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,1 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App'; // Importoni komponentin kryesor të aplikacionit tuaj
+import './index.css'; // Opsionale: mund të shtoni stilizime nëse është e nevojshme
+
+// Ky është vendi ku React do të "renderojë" aplikacionin tuaj dhe do ta lidhi me div-in me id 'root' në index.html
+ReactDOM.render(
+  <React.StrictMode>
+    <App />  {/* Komponenti kryesor */}
+  </React.StrictMode>,
+  document.getElementById('root') // Ky është div-i në index.html ku do të shfaqen komponentët tuaj
+);
 
